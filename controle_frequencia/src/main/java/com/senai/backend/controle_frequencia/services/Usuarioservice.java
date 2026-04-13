2e706d415c692ba1f3a1d996b7dbebf3cde04b5e
arthur7700 <cortasioarthur@gmail.com>
2026-04-13 16:33:14 -0300

    service

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/services/Usuarioservice.java
@@ -0,0 +1,72 @@
+package com.senai.backend.controle_frequencia.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.backend.controle_frequencia.models.Usuario;
+import com.senai.backend.controle_frequencia.repositories.Usuariorepository;
+
+@Service
+public class Usuarioservice {
+
+    @Autowired
+    private Usuariorepository Usuariorepository;
+
+    public Long contarUsuario() {
+
+        return Usuariorepository.count();
+
+    }
+
+    public Usuario buscarUsuario(Integer id) {
+
+        return Usuariorepository.findById(id).get();
+
+    }
+
+    public List<Usuario> listarUsuario() {
+
+        return Usuariorepository.findAll();
+
+    }
+
+    public Boolean deletarUsuario(Integer id) {
+
+        if (Usuariorepository.existsById(id)) {
+
+            Usuariorepository.deleteById(id);
+            return true;
+
+        }
+        return false;
+    }
+
+    public Usuario cadastrarUsuariol(Usuario usuario) {
+
+        return Usuariorepository.save(usuario);
+
+    }
+
+    public Usuario atualizarUsuario(Integer id, Usuario usuario) {
+
+        Usuario usuarioRecuperado = buscarUsuario(id);
+
+        if (usuarioRecuperado != null) {
+
+            usuarioRecuperado.setId(id);
+
+            if (usuario.getNome() != null) {
+                usuarioRecuperado.setNome(usuario.getNome());
+            }
+            if (usuario.getDataNascimento() != null) {
+
+                usuarioRecuperado.setDataNascimento(usuario.getDataNascimento());
+
+            }
+            return Usuariorepository.save(usuarioRecuperado);
+        }
+        return null;
+    }
+}
